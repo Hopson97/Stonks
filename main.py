123ceef26e92a5d64acc12adb777dd4f79457d77
Hopson97 <mhopson@hotmail.co.uk>
2020-07-01 18:07:33 +0100

    Add logo

--- main.py
+++ main.py
@@ -2,36 +2,42 @@ from os import system, name
 from time import sleep
 from random import randrange
 
-def clear(): 
-    print(chr(27) + "[2J")
+WIDTH = 175
+HEIGHT = 54
+
+STONKS = "\n\
+                _____ _______ ____  _   _ _  __ _____    _____ _____ __  __ _    _ _            _______ ____  _____  TM \n\
+               / ____|__   __/ __ \| \ | | |/ // ____|  / ____|_   _|  \/  | |  | | |        /\|__   __/ __ \|  __ \ \n\
+              | (___    | | | |  | |  \| | ' /| (___   | (___   | | | \  / | |  | | |       /  \  | | | |  | | |__) |\n\
+               \___ \   | | | |  | | . ` |  <  \___ \   \___ \  | | | |\/| | |  | | |      / /\ \ | | | |  | |  _  / \n\
+               ____) |  | | | |__| | |\  | . \ ____) |  ____) |_| |_| |  | | |__| | |____ / ____ \| | | |__| | | \ \ \n\
+              |_____/   |_|  \____/|_| \_|_|\_\_____/  |_____/|_____|_|  |_|\____/|______/_/    \_\_|  \____/|_|  \_\ \n"
 
 class Buffer:
     def __init__(self):
         self.buff = []
-        self.width = 125
-        self.height = 54
-        for _ in range(self.height):
-            for _ in range(self.width):
+        for _ in range(HEIGHT):
+            for _ in range(WIDTH):
                 self.buff += '.'
             self.buff += '\n'
-        for y in range(self.height):
-            val = str(self.height - y)
+        for y in range(HEIGHT):
+            val = str(HEIGHT - y)
             if len(val) > 1:
-                self.buff[y * self.width + y] = val[0]
-                self.buff[y * self.width + y + 1] = val[1]
+                self.buff[y * WIDTH + y] = val[0]
+                self.buff[y * WIDTH + y + 1] = val[1]
             else:
-                self.buff[y * self.width + y] = val
+                self.buff[y * WIDTH + y] = val
         
     def set(self, x, y, v):
-        self.buff[y * self.width + y + x] = v
+        self.buff[y * WIDTH + y + x] = v
 
     def print(self):
         print("".join(self.buff))
 
     def clear(self):
-        for y in range(self.height):
-            for x in range(3, self.width):
-                self.set(x, y, ' ')
+        for y in range(HEIGHT):
+            for x in range(3, WIDTH):
+                self.set(x, y, '.')
             
 class Stonk:
     def __init__(self, start):
@@ -50,14 +56,12 @@ class Stonk:
 buffer = Buffer()
 stonk = Stonk(0)
 buffer.clear()
-
-while True:
-    sleep(0.25)
+while len(stonk.history) < WIDTH - 3:
+    sleep(0.01)
+    print(STONKS)
     for idx, value in enumerate(stonk.history):
-        buffer.set(idx + 3, buffer.height - value - 1, '#')
-
+        buffer.set(idx + 3, HEIGHT - value - 1, '#')
     stonk.sim()
     buffer.print()
 
-    buffer.clear()
 
