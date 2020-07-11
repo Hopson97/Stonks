ad9dd0d30009d0513356dc11a7f0b0da9f012592
Tom <tomgrin10@gmail.com>
2020-07-11 23:52:45 +0300

    Make into a pip-package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='pystonks',
+    version='1.0',
+    description='stonks',
+    author='Hopson97',
+    author_email='',
+    url='https://github.com/Hopson97/Stonks/',
+    packages=['stonks'],
+    entry_points={
+        "console_scripts": [
+            "stonks = stonks:main"
+        ]
+    }
+)
